921603af910ebe01acbf0ea4f6f6d94b2542c540
ASUS <pranitbhopi1913@gmail.com>
2025-05-03 18:28:07 +0530

    Final_Commit

--- main/admin.py
+++ main/admin.py
@@ -6,3 +6,6 @@ admin.site.register(Photo)
 admin.site.register(inq)
 admin.site.register(Amenity)
 admin.site.register(agent_inquiry)
+admin.site.site_header = "NCPR Admin"
+admin.site.site_title = "NCPR Admin Portal"
+admin.site.index_title = "Welcome to NCPR Dashboard"
--- ncpr/urls.py
+++ ncpr/urls.py
@@ -20,7 +20,7 @@ from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('saurabh/', admin.site.urls),
     path('', include('main.urls')),
     
 
